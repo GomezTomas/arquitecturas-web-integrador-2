f1ac40ab7a7b97b44daf0c4b2d48ba2b326d8cbd
Tomas <tgomez@alumnos.exa.unicen.edu.ar>
2025-10-01 16:54:19 -0300

    repository factory

--- src/main/java/repository/implementacion/CarreraRepositoryImpl.java
+++ src/main/java/repository/implementacion/CarreraRepositoryImpl.java
@@ -6,24 +6,47 @@ import repository.CarreraRepository;
 import javax.persistence.EntityManager;
 
 public class CarreraRepositoryImpl implements CarreraRepository {
-    private EntityManager em;
-    @Override
-    public void create(Carrera elem) {
 
+    private EntityManager em;
+    private static CarreraRepositoryImpl instance = null;
+
+    private CarreraRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+    public static CarreraRepositoryImpl getInstance(EntityManager em) {
+        if (instance == null) {
+            instance = new CarreraRepositoryImpl(em);
+        }
+        return instance;
+    }
+
+    @Override
+    public void create(Carrera carrera) {
+        em.getTransaction().begin();
+        em.persist(carrera);
+        em.getTransaction().commit();
     }
 
     @Override
     public Carrera findById(int id) {
-        return null;
+        em.getTransaction().begin();
+        Carrera carrera = em.find(Carrera.class, id);
+        em.getTransaction().commit();
+        return carrera;
     }
 
     @Override
-    public void delete(Carrera elem) {
+    public void delete(Carrera carrera) {
+        em.getTransaction().begin();
+        em.remove(carrera);
+        em.getTransaction().commit();
 
     }
 
     @Override
-    public void update(Carrera elem) {
-
+    public void update(Carrera carrera) {
+        em.getTransaction().begin();
+        em.merge(carrera);
+        em.getTransaction().commit();
     }
 }
