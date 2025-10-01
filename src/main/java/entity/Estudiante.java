0ebea858e375d7ee4b8c807258b8010d55e92184
Facundo Castro <facucastrosl@gmail.com>
2025-10-01 16:49:17 -0300

    implementacion estudiante

--- src/main/java/entity/Estudiante.java
+++ src/main/java/entity/Estudiante.java
@@ -1,4 +1,97 @@
 package entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
 public class Estudiante {
+    @Id
+    private int DNI;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private int edad;
+    @Column
+    private String genero;
+    @Column
+    private String ciudad;
+    @Column
+    private int LU;
+
+    @OneToMany(mappedBy = "estudiante")
+    private List<EstudianteCarrera> estudianteCarrera;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int DNI, String nombre, String apellido, int edad, String genero, String ciudad, int LU) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.genero = genero;
+        this.ciudad = ciudad;
+        this.LU = LU;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getLU() {
+        return LU;
+    }
+
+    public void setLU(int LU) {
+        this.LU = LU;
+    }
 }
