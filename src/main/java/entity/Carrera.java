642aff9e9645c4bd12a3b3f8a2f199f9771d8f73
Melina Theill <meli.theill@gmail.com>
2025-10-01 16:27:08 -0300

    Carerra Entity

--- src/main/java/entity/Carrera.java
+++ src/main/java/entity/Carrera.java
@@ -1,4 +1,49 @@
 package entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Carrera {
+
+    @Id
+    @Column
+    private int id;
+    private String carrera;
+    private int duracion;
+
+     @ManyToOne
+     @JoinColumn
+     private EstudianteCarrera estudianteCarrera;
+
+    public Carrera() {
+    }
+    public Carrera(int id, String carrera, int duracion) {
+        this.id = id;
+        this.carrera = carrera;
+        this.duracion = duracion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
 }
