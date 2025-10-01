280d387ccb6248a9cd282922bf12b722ad01d707
Melina Theill <meli.theill@gmail.com>
2025-10-01 17:12:18 -0300

    Implementacion EstudianteRepositoryImpl

--- src/main/java/repository/implementacion/EstudianteRepositoryImpl.java
+++ src/main/java/repository/implementacion/EstudianteRepositoryImpl.java
@@ -1,30 +1,55 @@
 package repository.implementacion;
 
+import entity.Carrera;
 import entity.Estudiante;
+import repository.CarreraRepository;
 import repository.EstudianteRepository;
 
 import javax.persistence.EntityManager;
 
 public class EstudianteRepositoryImpl implements EstudianteRepository {
     private EntityManager em;
+    private static EstudianteRepository instance = null;
+
+    private EstudianteRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    public static EstudianteRepository getInstance(EntityManager em) {
+        if (instance == null) {
+            instance = new EstudianteRepositoryImpl(em);
+        }
+        return instance;
+    }
+
+
 
     @Override
     public Estudiante findById(int id) {
-        return null;
+        em.getTransaction().begin();
+        Estudiante estudiante = em.find(Estudiante.class, id);
+        return estudiante;
     }
 
     @Override
-    public void create(Estudiante elem) {
-
+    public void create(Estudiante estudiante) {
+       em.getTransaction().begin();
+       em.persist(estudiante);
+       em.getTransaction().commit();
     }
 
     @Override
-    public void delete(Estudiante elem) {
-
+    public void delete(Estudiante estudiante) {
+       em.getTransaction().begin();
+       em.remove(estudiante);
+       em.getTransaction().commit();
     }
 
     @Override
-    public void update(Estudiante elem) {
+    public void update(Estudiante estudiante) {
+       em.getTransaction().begin();
+       em.merge(estudiante);
+       em.getTransaction().commit();
 
     }
 }
