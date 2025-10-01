e78b197466bab3d657edc2789bfecdaff048ec39
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2025-10-01 16:17:59 -0300

    Entity EstudianteCarrera con atributos, constructor vacio, constructor con campos, getters setters

--- src/main/java/entity/EstudianteCarrera.java
+++ src/main/java/entity/EstudianteCarrera.java
@@ -1,4 +1,68 @@
 package entity;
 
 public class EstudianteCarrera {
+    private int id;
+    private int idEstudiante;
+    private int idCarrera;
+    private int inscripcion;
+    private int graduacion;
+    private int antiguedad;
+    public EstudianteCarrera(){}
+
+    public EstudianteCarrera(int id, int idEstudiante, int idCarrera, int inscripcion, int graduacion, int antiguedad) {
+        this.id = id;
+        this.idEstudiante = idEstudiante;
+        this.idCarrera = idCarrera;
+        this.inscripcion = inscripcion;
+        this.graduacion = graduacion;
+        this.antiguedad = antiguedad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public int getInscripcion() {
+        return inscripcion;
+    }
+
+    public void setInscripcion(int inscripcion) {
+        this.inscripcion = inscripcion;
+    }
+
+    public int getGraduacion() {
+        return graduacion;
+    }
+
+    public void setGraduacion(int graduacion) {
+        this.graduacion = graduacion;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
 }
